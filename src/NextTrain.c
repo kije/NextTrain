b92c06c2443b7cc42f030b04dcbb61f79b3697d4
Kim Jeker <info@kije.ch>
2013-10-19 09:07:39 -0700

    sync

--- src/NextTrain.c
+++ src/NextTrain.c
@@ -3,17 +3,17 @@
 #include "pebble_fonts.h"
 
 
-#define MY_UUID { 0x5A, 0x69, 0xC1, 0x98, 0xD2, 0x73, 0x4D, 0x10, 0xBB, 0xA3, 0xEC, 0x91, 0x7B, 0x3F, 0xB9, 0x65 }
+#define MY_UUID { 0x5B, 0x08, 0xBC, 0x25, 0xA5, 0x8E, 0x4E, 0x1C, 0xB4, 0xC5, 0x2A, 0x22, 0x34, 0xAB, 0xDF, 0x85 }
 PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
-             1, 0, /* App version */
+             "NextTrain", "kije dev",
+             0, 1, /* App version */
              DEFAULT_MENU_ICON,
-             APP_INFO_STANDARD_APP);
+             APP_INFO_WATCH_FACE);
 
-Window window;
 
 
 void handle_init(AppContextRef ctx) {
+  (void)ctx;
 
   window_init(&window, "Window Name");
   window_stack_push(&window, true /* Animated */);
