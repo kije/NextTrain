127c3d82b99cdb3b982c64a49a687b6f50e19312
Kim Jeker <info@kije.ch>
2013-10-22 13:45:19 -0700

    Added ICON

--- src/NextTrain.c
+++ src/NextTrain.c
@@ -7,10 +7,10 @@
 
 
 #define MY_UUID { 0x5B, 0x08, 0xBC, 0x25, 0xA5, 0x8E, 0x4E, 0x1C, 0xB4, 0xC5, 0x2A, 0x22, 0x34, 0xAB, 0xDF, 0x85 }
-PBL_APP_INFO(MY_UUID,
+PBL_APP_INFO(HTTP_UUID,
              "NextTrain", "kije dev",
              0, 1, /* App version */
-             DEFAULT_MENU_ICON,
+             RESOURCE_ID_CUSTOM_APP_ICON_WHITE,
              APP_INFO_WATCH_FACE);
 
 
