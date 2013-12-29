1d5b5ff311add3b7e473a6a991f981dfde64dfc9
Kim Jeker <info@kije.ch>
2013-12-29 05:27:07 -0800

    Prepared for SDK 2.0

--- src/NextTrain.c
+++ src/NextTrain.c
@@ -1,32 +1,19 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
-#include "http.h"
-
-#define SERVER_URL "http://kije.cetus.uberspace.de/serv/NextTrain/NextTrainServer/"
+#include <pebble.h>
+#include "header.h"
 
 
-#define MY_UUID { 0x5B, 0x08, 0xBC, 0x25, 0xA5, 0x8E, 0x4E, 0x1C, 0xB4, 0xC5, 0x2A, 0x22, 0x34, 0xAB, 0xDF, 0x85 }
-PBL_APP_INFO(HTTP_UUID,
-             "NextTrain", "kije dev",
-             0, 1, /* App version */
-             RESOURCE_ID_CUSTOM_APP_ICON_WHITE,
-             APP_INFO_WATCH_FACE);
-
-
-Window window;
-
-void handle_init(AppContextRef ctx) {
-  (void)ctx;
-
-  window_init(&window, "Window Name");
-  window_stack_push(&window, true /* Animated */);
+static void init(void) {
+}
+	
+void deinit(void) {
 }
 
 
-void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init
-  };
-  app_event_loop(params, &handlers);
+int main(void) {
+	app_log(APP_LOG_LEVEL_DEBUG, __FILE__ , __LINE__ , "Start");
+	init();
+	app_event_loop();
+	deinit();
+	app_log(APP_LOG_LEVEL_DEBUG, __FILE__ , __LINE__ , "Done");
+	return 0;
 }
---
+++ src/header.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+	
+#define SERVER_URL "http://kije.cetus.uberspace.de/serv/NextTrain/NextTrainServer/"
+	
+#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
+	
+#endif
